1b1807487ece025538815987600cdf51e7d84f92
unknown <leandro.wsp@gmail.com>
2022-10-13 20:04:38 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,22 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		
 		Department obj = new Department(1,"Books");
+		
+		Seller seller = new Seller(38, "Leandro", "leandro.wsp@gmail.com", new Date(), 7000.0, obj);
+		
 		
 		System.out.println(obj);
 		
+		System.out.println(seller);
 		
 		
 		
