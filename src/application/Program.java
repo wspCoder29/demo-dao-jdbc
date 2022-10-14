6859121a02049bd1cf4ea8c083f971b15c8930e5
unknown <leandro.wsp@gmail.com>
2022-10-13 22:08:50 -0300

    SellerDao JDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		Department obj = new Department(1,"Books");
 		
 		Seller seller = new Seller(38, "Leandro", "leandro.wsp@gmail.com", new Date(), 7000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		
 		System.out.println(obj);
